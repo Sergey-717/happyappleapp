14dbade85fdefc7edef31225f18044ba6793e9dc
Сергей Казаков <sergey717@MacBook-Air-Sergej-2.local>
2023-02-13 18:42:16 +0600

    happy 13 of february 5

--- pages/admin.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-// const App = dynamic(() => import("../src/admin/App"), { ssr: false });
-
-// const AdminPage = () => {
-//   return <App />;
-// };
-
-// export default AdminPage;
